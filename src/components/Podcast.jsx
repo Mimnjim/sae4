219c0a809dc6ae86c26c161d1875f025d5e78eec
Mimnjim <tejimmy77@gmail.com>
2026-03-25 11:18:21 +0100

    Choix de différentes couleurs pour le triangle dans la section "ExperienceSection"

--- src/components/Podcast.jsx
+++ src/components/Podcast.jsx
@@ -4,6 +4,8 @@ export default function Podcast() {
         <div className="podcast-container">
             <h2>Podcast</h2>
             <p>Découvrez notre podcast exclusif, où nous explorons les coulisses de l'exposition "Manga Archive". Plongez dans des discussions passionnantes avec les artistes, les conservateurs et les experts du manga, et obtenez des insights uniques sur les œuvres présentées. Disponible sur toutes les plateformes de streaming.</p>
+            <audio src=""></audio>
+
         </div>
 
     )
